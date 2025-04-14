c80149c912aa90c3ebeff4df9c6fda7c010c4113
JuleeP <juleeperween34@gmail.com>
2025-04-14 01:47:11 -0700

    rebuild

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // CORS Configuration to allow multiple origins
 app.use(cors({
-    origin: ['http://localhost:3000'],  // Multiple allowed origins
+    origin: ['http://localhost:3000', 'https://credenthealthadmin.vercel.app'],  // Multiple allowed origins
     methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allowed HTTP methods
     credentials: true  // Allow cookies if needed
   }));
